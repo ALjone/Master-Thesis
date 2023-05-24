b5ad5c02d41cbe8439fdc5ff449fcf3877618684
Audun <audun.henriksen@gmail.com>
2023-05-24 17:23:26 +0200

    More bug fix

--- GPY.py
+++ GPY.py
@@ -141,7 +141,7 @@ class GP:
 
         with torch.no_grad():
             # Compute Z scores
-            biggest = torch.amax(self.y[idx], dim=(1)).unsqueeze(1).unsqueeze(1).cpu()
+            biggest = torch.amax(self.y, dim=(1)).unsqueeze(1).unsqueeze(1).cpu()
             e = 0.001 #TODO: Hyperparameter tune e
             Z = (mean - biggest - e) / std
 
